346a749d4ee0516d2482b4b118b5247ba55ffd0a
Jayda <jayda@JayDavinci.com>
2024-11-09 20:56:33 +0700

    Tugas Pendahuluan 2

---
+++ MODUL2_JAY/TPMODUL2_JAY/src/Burung.java
@@ -0,0 +1,19 @@
+class Burung extends Hewan {
+   private String warnaBulu;
+
+   public Burung(String nama, int umur, String warnaBulu) {
+      super(nama, umur);
+      this.warnaBulu = warnaBulu;
+   }
+
+   @Override
+   public void suara() {
+      System.out.println(nama + " berkicau.");
+   }
+
+   @Override
+   public void infoHewan() {
+      System.out.println("Nama: " + nama + ", Umur: " + umur + " tahun.");
+      System.out.println("Warna bulu: " + warnaBulu);
+   }
+}
---
+++ MODUL2_JAY/TPMODUL2_JAY/src/Kucing.java
@@ -0,0 +1,19 @@
+class Kucing extends Hewan {
+   private String ras;
+
+   public Kucing(String nama, int umur, String ras) {
+      super(nama, umur);
+      this.ras = ras;
+   }
+
+   @Override
+   public void suara() {
+      System.out.println(nama + " mengeong.");
+   }
+
+   @Override
+   public void infoHewan() {
+      System.out.println("Nama: " + nama + ", Umur: " + umur + " tahun.");
+      System.out.println("Ras: " + ras);
+   }
+}
